00d67f115892c4bc45c4916220a29a275dba2401
jueunhong <85168145+jueunhong@users.noreply.github.com>
2023-02-07 23:02:07 +0900

    types part 2

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -49,3 +49,52 @@ const playerMaker2 = (name:string): Player => ({name})
 const lynn = playerMaker2("lynn")
 lynn.age = 12
 
+
+//6. readonly
+const names: readonly string[] = ["1", "2"]
+//error: names.push("3")
+
+//7. tuple
+const player: [string, number, boolean] = ["nico", 1, true]
+//error: player[0] = 1
+
+
+//8. any - escape from typescript
+//not good
+const any1 : any[] = [1, 2, 3]
+const any2 : any = "nico"
+any1 + any2
+
+//9. unknown
+let u: unknown;
+
+if(typeof u === 'number'){
+    let u2 = u + 1
+}
+
+if(typeof u === "string"){
+    let u3 = u.toUpperCase
+    
+}
+
+//10. void
+function hello(){
+    console.log('x')
+}
+
+
+//11. never -> never return function
+function hello2(): never{
+    throw new Error("xx")
+}
+
+function hello3(name: string|number){
+    if(typeof name === 'string'){
+        name
+    } else if (typeof name === 'number'){
+        name
+    } else {
+        name
+    }
+}
+
